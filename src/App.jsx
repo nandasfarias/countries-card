dac6b75ea3df522c28c498a1c6274c9f5b8d870e
fernandasfarias <fernandafariass734@gmail.com>
2026-02-03 15:10:48 -0300

    feat: create project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-[#F0F0DB]">
+      <p>Hello</p>
+    </div>
+  )
+}
+
+export default App
